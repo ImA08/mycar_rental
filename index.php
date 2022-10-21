86658f87578f12b7ced8f2393c536b79816b63a9
Imanul Aufa <imanulaufa@gmail.com>
2022-10-22 02:44:14 +0700

    Signed-off-by: Imanul Aufa <imanulaufa@gmail.com>
    "Membuat file index dan style"
    "Membuat bagian navigasi"

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyCar Rental</title>
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- header section -->
+
+    <header class="header">
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+        <a href="#" class="logo"> <i>My<span>Car</span>Rental</i></a>
+        <nav class="navbar">
+            <a href="#">Our Services</a>
+            <a href="#">Why Us?</a>
+            <a href="#">Testimonials</a>
+            <a href="#">FAQs</a>
+        </nav>
+
+        <div id="login-btn">
+            <button class="btn">Login</button>
+            <i class="far fa-user"></i>
+        </div>
+    </header>
+
+</body>
+
+</html>
